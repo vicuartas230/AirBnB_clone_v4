998024a051b4f4befb119763056fb3cfdeee513d
dianaparr <dianaparra.9611@gmail.com>
2021-09-23 17:01:25 -0500

    Uploading new update in both files to task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $('UL LI INPUT:checkbox').change(() => {
-        let storeAmenity = dict()
+    const storeAmenity = {};
+    const listStore = [];
+    let idx;
+    $('UL LI INPUT:checkbox').change(function () { // ("input[type=checkbox]")
         if ($(this).is(':checked')) {
-            storeAmenity[$(this).attr('data-id')] = $(this).attr('data-name');
+            storeAmenity[$(this).attr('data-id')] = $(this).attr('data-name'); // or .data('name')
+            listStore.push(Object.values(storeAmenity));
+            //alert('hola');
         } else {
-            alert('unchecked');
+            console.log(listStore)
+            idx = listStore.indexOf('Iron');
+            console.log(idx)
+            delete listStore[idx];
         }
+        //$('.amenities H4').text('hola')
+        $('.amenities H4').text(listStore);
     });
 });
