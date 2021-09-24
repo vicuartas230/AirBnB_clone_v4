960a5c19b1e4f6ecb5f033ce90df2f3580cec276
victor <vicuartas230@gmail.com>
2021-09-23 22:26:03 -0500

    Upload files of task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const storeAmenity = {};
+    const listStore = Array();
+    let idx;
+    $('UL LI INPUT:checkbox').change(function () { // ("input[type=checkbox]")
+        if ($(this).is(':checked')) {
+            storeAmenity[$(this).attr('data-id')] = $(this).attr('data-name'); // or .data('name')
+            listStore.push(Object.values(storeAmenity)[0]);
+        } else {
+            idx = listStore.indexOf($(this).attr('data-name'));
+            listStore.splice(idx, 1);
+        }
+        $('.amenities H4').html(listStore.join(', '));
+    });
+    $.getJSON("http://127.0.0.1:5001/api/v1/status", function (data) {
+            if (data.status === 'OK') {
+                $('div#api_status').addClass('available');
+            } else {
+                $('div#api_status').removeClass('available');
+            }
+        }
+    );
+    $.getJSON("http://0.0.0.0:5001/api/v1/places_search/"), function (data) {
+
+    }
+});
